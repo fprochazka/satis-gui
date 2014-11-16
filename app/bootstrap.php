1c1c029d8b37ebd6e061015c84a27a81ffaf86c2
Filip Procházka <filip@prochazka.su>
2014-11-17 00:06:58 +0100

    cleanup

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,37 +1,23 @@
 <?php
 
 use Nette\Configurator;
-use Nette\Utils\Strings;
 
-require __DIR__ . '/shortcuts.php';
 require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Configurator;
 
-$debugMode = FALSE;
-
-$debugSwitchFile = __DIR__ . '/config/debug';
-if (file_exists($debugSwitchFile)) {
-	$debugMode = Strings::trim(mb_strtolower(file_get_contents($debugSwitchFile))) === 'true' ? TRUE : FALSE;
-}
-
-$configurator->setDebugMode($debugMode);
 $configurator->enableDebugger(__DIR__ . '/../log');
-
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
-$configurator->createRobotLoader()
+if ($configurator->isDebugMode()) {
+	$configurator->createRobotLoader()
 		->addDirectory(__DIR__)
 		->register();
+}
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
-
-$localConfig = __DIR__ . '/config/config.local.neon';
-
-if (file_exists($localConfig)) {
+if (file_exists($localConfig = __DIR__ . '/config/config.local.neon')) {
 	$configurator->addConfig($localConfig);
 }
 
-$container = $configurator->createContainer();
-
-return $container;
+return $configurator->createContainer();
