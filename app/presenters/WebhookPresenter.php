eed814e1bca4699735cbfb5e51bb1012c50b543f
Filip Procházka <filip@prochazka.su>
2014-11-17 03:45:34 +0100

    Log webhook calls

--- app/presenters/WebhookPresenter.php
+++ app/presenters/WebhookPresenter.php
@@ -3,6 +3,7 @@
 namespace App\Presenters;
 
 use App\Model\Builder;
+use Tracy\Debugger;
 
 
 
@@ -23,11 +24,30 @@ class WebhookPresenter extends BasePresenter
 	}
 
 
+
+	protected function startup()
+	{
+		parent::startup();
+		Debugger::$productionMode = TRUE;
+	}
+
+
+
 	public function actionDefault($password)
 	{
 		if ($password === $this->context->parameters['webhook']['password']) {
 			$this->builder->build();
 		}
+
+		$logDir = $this->context->expand('%logDir%/api-calls.log');
+
+		$request = '[' . date('Y-m-d H:i:s') . "]\n";
+		foreach ($this->getHttpRequest()->getHeaders() as $header => $value) {
+			$request .= $header . ': ' . $value . "\n";
+		}
+		$request .= $this->getHttpRequest()->getRawBody() . "\n\n";
+		file_put_contents($logDir, $request, FILE_APPEND);
+
 		$this->terminate();
 	}
 
